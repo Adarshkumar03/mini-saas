29fdd62c21ea6ebaf8f0692eda9bf368edbf9820
Adarsh Kumar <adarshkumar245@gmail.com>
2025-07-05 20:57:55 +0530

    update playwright.config.ts

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
 	webServer: {
 		command: 'npm run build && npm run preview',
 		port,
-		reuseExistingServer: !process.env.CI,
+		reuseExistingServer: true,
 		timeout: 120 * 1000
 	},
 	testDir: 'e2e'
