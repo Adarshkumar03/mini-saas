4afc6b1a80b3b16b94aaced326ea1053dfb9fbd5
Adarsh Kumar <adarshkumar245@gmail.com>
2025-07-06 16:30:48 +0530

    fix: format code with Prettier

--- frontend/src/routes/issues/[issueId]/+page.svelte
+++ frontend/src/routes/issues/[issueId]/+page.svelte
@@ -259,7 +259,7 @@
 							{#await renderMarkdown(issue.description)}
 								<p class="text-gray-500">Loading description...</p>
 							{:then sanitizedHtml}
-								<!-- @ts-ignore -->
+								<!-- eslint-disable-next-line svelte/no-html-tag -->
 								{@html sanitizedHtml}
 							{/await}
 						</div>
